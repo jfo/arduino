9a015ead230e17995ef0ce5e9d6dc9accf0a8446
urthbound <jeffowler@gmail.com>
2015-02-01 21:26:04 -0500

    tty comm works

--- nonblocking/src/sketch.ino
+++ nonblocking/src/sketch.ino
@@ -5,15 +5,29 @@ unsigned long interval = 4545/2;
 
 void setup() {
     pinMode(outPin, OUTPUT);
+    Serial.begin(9600);
 }
 
 int amp = 0;
 unsigned long currentMicros;
+boolean on = false;
 
 void loop()
 {
+    if (Serial.available()) {
+
+        Serial.read();
+
+        if (on) {
+            on = false;
+        } else if (!on) {
+            on = true;
+        }
+    }
+
     currentMicros = micros();
-    if(currentMicros - previousMicros > interval) {
+
+    if ((currentMicros - previousMicros > interval) && on) {
         previousMicros = currentMicros;
 
         if (amp == HIGH) {
